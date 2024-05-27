274ea2a4aa8357c2336a135d5796479c66f85664
MarianoGarcia24 <mariano.garciae24@gmail.com>
2024-05-27 13:24:44 -0300

    added new client example

--- HomeBankingMindHub/Models/DBInitializer.cs
+++ HomeBankingMindHub/Models/DBInitializer.cs
@@ -11,7 +11,8 @@
                     new Client{ FirstName="Kobe", LastName="Bryant", email= "kobe23@gmail.com", password="123" },
                     new Client{ FirstName="Michael B", LastName="Jordan", email= "LebronJames@gmail.com", password="123" },
                     new Client{ FirstName="Juan Roman", LastName="Riquelme", email= "jrr10@gmail.com", password="123" },
-                    new Client{ FirstName="Martin", LastName="Palermo", email= "mp9@gmail.com", password="123" }
+                    new Client{ FirstName="Martin", LastName="Palermo", email= "mp9@gmail.com", password="123" },
+                    new Client{ FirstName="Sebastian", LastName="Battaglia", email= "labattaglioneta@gmail.com", password="123" }
                 };
 
                 context.Clients.AddRange(clients);
